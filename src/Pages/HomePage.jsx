50dcbd3d6f84290542db1cbf2a3bd630ad7faa96
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-05-18 00:51:54 +0600

    banner implemented

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Components/Banner';
 
 const HomePage = () => {
     return (
         <div>
-            <h1>Home page</h1>
+            <Banner></Banner>
             
         </div>
     );
