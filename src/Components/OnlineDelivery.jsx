8bc21789f333a922a103ce4773c56d60171c334a
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-05-19 04:36:36 +0600

    extra section in home page

--- src/Components/OnlineDelivery.jsx
+++ src/Components/OnlineDelivery.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const OnlineDelivery = () => {
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-delivery-concept_23-2149154530.jpg?w=1060&t=st=1684448434~exp=1684449034~hmac=03dcabeed741617295d48eb2712d155a0b8cc8abfbaf5d70eb88a840edd8baad" className="max-w-sm rounded-lg shadow-2xl" />
+        <div className='container mx-auto my-32'>
+            <div className=" ">
+                <div className=" lg:flex justify-center gap-10 space-y-3 lg:flex-row">
+                    <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-delivery-concept_23-2149154530.jpg?w=1060&t=st=1684448434~exp=1684449034~hmac=03dcabeed741617295d48eb2712d155a0b8cc8abfbaf5d70eb88a840edd8baad" className="lg:max-w-lg rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className="text-5xl font-bold">Online Delivery Available !!!</h1>
+                        <p className="py-6 text-lg ">Get it Delivered, Stress-Free: Sit back, relax, and let us handle the delivery. Enjoy the convenience of having your purchases brought right to your doorstep.</p>
+                        <button className="btn btn-primary">Order Now</button>
                     </div>
                 </div>
             </div>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,14 +3,15 @@ import Banner from '../Components/Banner';
 import ToyTabs from '../Components/ToyTabs';
 import GallerySection from '../Components/GallerySection';
 import GallerySec from '../Components/GallerySec';
+import OnlineDelivery from '../Components/OnlineDelivery';
 
 const HomePage = () => {
     return (
         <div>
             <Banner></Banner>
             <GallerySection></GallerySection>
-            
             <ToyTabs></ToyTabs>
+            <OnlineDelivery></OnlineDelivery>
             
         </div>
     );
